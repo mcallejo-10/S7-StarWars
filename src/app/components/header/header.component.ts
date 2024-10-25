bfe2007721cb9930055206486478d27a7271cd94
Miranda <mirandacallejonh@gmail.com>
2024-10-25 11:53:19 +0200

    header navbar added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, HomeComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
