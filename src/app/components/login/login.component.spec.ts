89b5a617375c35c9b676b3fbaa4618ce7ffa0308
Miranda Callejón <mirandacallejonh@gmail.com>
2024-11-05 22:39:49 +0100

    2 test al login afegits

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -2,20 +2,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject, of } from 'rxjs';
+import { LoginResponse } from '../../interfaces/login';
+import { signal, WritableSignal } from '@angular/core';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let httpTestingController: HttpTestingController;
-
+  let authService: AuthService;
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent],
+      imports: [LoginComponent, ReactiveFormsModule],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
+        
         { provide: ActivatedRoute, useValue: {} }
       ]
     })
@@ -23,10 +30,35 @@ describe('LoginComponent', () => {
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  it('should have a valid login form', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+    component.loginForm.controls['email'].setValue('test@example.com');
+    component.loginForm.controls['password'].setValue('password');
+    expect(component.loginForm.valid).toBeTruthy();
+  });
+
+  
+  it('should handle email not found', () => {
+    spyOn(authService, 'checkEmailExists').and.returnValue(of([]));
+    spyOn(router, 'navigate');
+
+    component.checkEmailLogin();
+    
+    expect(component.userExist).toBeFalsy();
+    expect(component.isValidEmail).toBeTruthy();
+    expect(router.navigate).toHaveBeenCalledWith(['/register']);
+  });
+
 });
